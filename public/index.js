b46ee207a7be7a163aa4b79868ee3be9b3721c1e
Nandu Krishna T <nandukrishnat3@gmail.com>
2021-02-13 12:08:47 +0530

    New Layout Using Tailwind CSS

---
+++ public/index.js
@@ -0,0 +1,18 @@
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+
+burger.addEventListener('click', () =>{
+    if(menu.classList.contains('hidden')){
+        menu.classList.remove('hidden');
+    }else{
+        menu.classList.add('hidden');
+    }
+})
+
+menu.addEventListener('click', () =>{
+    if(menu.classList.contains('hidden')){
+        menu.classList.remove('hidden');
+    }else{
+        menu.classList.add('hidden');
+    }
+})
